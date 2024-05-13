d504e04413fd83a9bea2b46f9236d527f34670b6
VietHa <havietnguyen2002@gmail.com>
2024-05-13 10:27:04 +0700

    Web demo

--- src_ref/Durian_webInfor/main.js
+++ src_ref/Durian_webInfor/main.js
@@ -1,31 +1,48 @@
-var valueDayExport = firebase.database().ref("sample_1").child("date of exportation");
-// valueTemp.on("value", (snap) => {
+// Hàm để lấy giá trị của một tham số từ URL
+function getQueryParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+// Lấy giá trị của tham số "custom_param" từ URL
+const customParamValue = getQueryParam('custom_param');
+
+// Hiển thị giá trị của tham số "custom_param" lên trang web
+// const customParamElement = document.createElement('p');
+// customParamElement.textContent = 'Giá trị của tham số custom_param là: ' + customParamValue;
+// document.body.appendChild(customParamElement);
+
+
+
+var valueDayExport = firebase.database().ref(customParamValue).child("date of exportation");
+valueTemp.on("value", (snap) => {
   console.log("Day: " + snap.val());
   document.getElementById("day").innerHTML = snap.val();
-// });
+});
 
 
 
-var valueOrgin = firebase.database().ref("sample_1").child("orgin");
-// valueTemp.on("value", (snap) => {
+var valueOrgin = firebase.database().ref(customParamValue).child("orgin");
+valueTemp.on("value", (snap) => {
   console.log("the orgin: " + snap.val());
   document.getElementById("orgin").innerHTML = snap.val() ;
-// });
+});
 
-var valueHum = firebase.database().ref("sample_1").child("durian's name");
-// valueHum.on("value", (snap) => {
+var valueHum = firebase.database().ref(customParamValue).child("durian's name");
+valueHum.on("value", (snap) => {
   console.log("durian's name: " + snap.val());
   document.getElementById("name").innerHTML = snap.val() ;
-// });
-
-var valueHum = firebase.database().ref("sample_1").child("weight");
-// valueHum.on("value", (snap) => {
+});
+// 
+var valueHum = firebase.database().ref(customParamValue).child("weight");
+valueHum.on("value", (snap) => {
   console.log("trọng lượng: " + snap.val());
   document.getElementById("weight").innerHTML = snap.val() ;
-// });
+});
 
-var valuepH = firebase.database().ref("sample_1").child("type");
-// valuepH.on("value", (snap) => {
+var valuepH = firebase.database().ref(customParamValue).child("type");
+valuepH.on("value", (snap) => {
   console.log("Type: " + snap.val());
   document.getElementById("type").innerHTML = snap.val() ;
-// });
+});
