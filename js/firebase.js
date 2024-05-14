93121adaccea75d74702369a343a635d49f9cab2
VietHa <havietnguyen2002@gmail.com>
2024-05-15 01:35:28 +0700

    Done Show Infor

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = { 
-  apiKey : "AIzaSyCj8R0iJmoT-hlfETLGdTYxzk5VUQ9CLBw" , 
-  authDomain : "mechatronic-project-af507.firebaseapp.com" , 
-  databaseURL : "https://mechatronic-project-af507-default-rtdb.firebaseio.com" , 
-  projectId : "mechatronic-project-af507" , 
-  storageBucket : "mechatronic-project-af507.appspot.com" , 
-  messagingSenderId : "782997268535" , 
-  appId : "1:782997268535:web:0f36553a1637a1400977b2" 
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ8oUAutO4fWgP3m8zOwDU01AJH0y26bI",
+  authDomain: "duriandb.firebaseapp.com",
+  databaseURL: "https://duriandb-default-rtdb.firebaseio.com",
+  projectId: "duriandb",
+  storageBucket: "duriandb.appspot.com",
+  messagingSenderId: "182817181684",
+  appId: "1:182817181684:web:02fc7a5a16d9e9ebd173a1",
+  measurementId: "G-Z4P58Y05SY"
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+// const analytics = getAnalytics(app);
