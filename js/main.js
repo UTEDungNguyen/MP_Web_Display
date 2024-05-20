892fc32ec492526cefbafd86e9a18b47bbb8d7e2
Dung Nguyen <dungduide2002@gmail.com>
2024-05-21 02:24:54 +0700

    update: testing after modify code

--- js/main.js
+++ js/main.js
@@ -1,32 +1,20 @@
-// Hàm để lấy giá trị của một tham số từ URL
+// Get param from URL
 function getQueryParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
 
-// // Lấy giá trị của tham số "custom_param" từ URL
+// Get Value "custom_param" from URL
 const customParamValue = getQueryParam('custom_param');
 
-// // Hiển thị giá trị của tham số "custom_param" lên trang web
-// const customParamElement = document.createElement('p');
-// customParamElement.textContent = 'Giá trị của tham số custom_param là: ' + customParamValue;
-// document.body.appendChild(customParamElement);
-
 console.log("custom param value: " + customParamValue);
-
-// $(document).ready(function(){
-//   alert(1);
-// });
-
 
 var valueDayExport = firebase.database().ref(customParamValue).child("Date_Export");
 valueDayExport.on("value", (snap) => {
   console.log("Day: " + snap.val());
   document.getElementById("date").innerHTML = snap.val();
 });
-
-
 
 var valueOrgin = firebase.database().ref(customParamValue).child("Orgin");
 valueOrgin.on("value", (snap) => {
@@ -39,7 +27,7 @@ valueName.on("value", (snap) => {
   console.log("durian's name: " + snap.val());
   document.getElementById("name").innerHTML = snap.val() ;
 });
-// // 
+
 var valueWeight = firebase.database().ref(customParamValue).child("Weight");
 valueWeight.on("value", (snap) => {
   console.log("trọng lượng: " + snap.val());
@@ -52,27 +40,12 @@ valueType.on("value", (snap) => {
   document.getElementById("type").innerHTML = snap.val() ;
 });
 
-
-// // Reference to a specific image file
-// const storageRef = storage.ref();
-// const imgRef = storageRef.child(customParamValue +".JPG");
-
-// // Get the download URL
-// imgRef.getDownloadURL().then((url) => {
-//     // Insert url into an <img> tag to display the image
-//     const imgElement = document.getElementById('IMG');
-//     imgElement.src = url;
-// }).catch((error) => {
-//     console.error("Error getting download URL:", error);
-// });
-
 // Reference to a specific image file
 const storageRef = storage.ref();
-const imgRef = storageRef.child("Durian_bg.jpg");
+const imgRef = storageRef.child(customParamValue +".JPG");
 
 // Get the download URL
 imgRef.getDownloadURL().then((url) => {
-    console.log('File available at', url);
     // Insert url into an <img> tag to display the image
     const imgElement = document.getElementById('IMG');
     imgElement.src = url;
